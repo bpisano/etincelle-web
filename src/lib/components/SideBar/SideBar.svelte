e6ea69528e41d4c46d57b44eff540bb4eecd8556
bpisano <benjamin.pisano@icloud.com>
2026-02-12 13:55:22 +0100

    added font

---
+++ src/lib/components/SideBar/SideBar.svelte
@@ -0,0 +1,6 @@
+<ul class="w-full max-w-64 p-4">
+	<li>Home</li>
+	<li>Inventory</li>
+	<li>Lists</li>
+	<li>Profile</li>
+</ul>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
 	import { NavBar } from '$lib/components/NavBar';
+	import { SideBar } from '$lib/components/SideBar';
 	import './layout.css';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<title>Etincelle</title>
+	<meta name="description" content="Etincelle Web App" />
+	<link rel="icon" href={favicon} />
+</svelte:head>
 <div>
 	<NavBar />
-	{@render children()}
+	<div class="hstack">
+		<SideBar />
+		{@render children()}
+	</div>
 </div>
